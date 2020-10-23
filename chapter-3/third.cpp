e8d6b05504dfac6861bf712014e710d5ef5dc3c5
mridul-netizen <mridulsetia18@gmail.com>
2020-10-23 19:15:29 +0530

    third.cpp

--- chapter-3/third.cpp
+++ chapter-3/third.cpp
@@ -6,27 +6,111 @@ using namespace std;
 #define STACK_CAP 5//capacity of stack
 
 vector<stack<int>*> vec;//kya lawda macha diya ye bhaisaab
+int index = 0;
+bool initialized = false;
 
 void init_stacks() {
-
+    stack<int> *s = new stack<int>;
+    vec.push_back(s);
 }
 
 void stack_push(int v) {
-
+    if(initialized == false) {
+        initialized = true;
+        init_stacks();
+    }
+    stack<int> *s = vec.at(index);
+    if(s->size() == 5) {
+        s = new stack<int>;
+        vec.push_back(s);
+        index++;
+    }
+    s->push(v);
 }
 
 void stack_pop() {
-
+    stack<int> *s = vec.at(index);
+    if(index > 0) {
+        if(s->size() == 0) {
+            delete s;
+            index--;
+            s = vec.at(index);
+        }
+        s->pop();
+    }
+    else {
+        if(s->size() == 0) {
+            cout<<"No more elements in the stack!"<<endl;
+        }
+        else{
+            s->pop();
+        }
+    }
 }
 
-bool stack_top() {
-
+bool stack_top(int &v) {
+    //int v = 0;
+    stack<int> *s = vec.at(index);
+    if(s->size() == 0 && index == 0) {
+        return false;
+    }
+    if(s->size() == 0) {
+        delete s;
+        index--;
+        s = vec.at(index);
+    }
+    v = s->top();
+    return true;
 }
 
 void deinit_stacks() {
-
+    stack<int> *s;
+    for (int  i = 0; i <= index; i++)
+    {
+        s = vec.at(i);
+        delete s;
+    }
+    
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    init_stacks();
+    for (int i = 0; i < 30; i++)
+    {
+        stack_push(i);
+        cout<<"Push:Stack Index "<<index<<"--"<<i<<endl;
+    }
+    for (int i = 0; i < 40; i++)
+    {
+        int v;
+        int b = stack_top(v);
+        stack_pop();
+        if(b) {
+            cout<<"Stack index"<<index<<"--"<<v<<endl;
+        }
+        else {
+            cout<<"No more elements in the stack!"<<endl;
+        }
+    }
+    for(int i = 0;i<10;i++) {
+        stack_push(i);
+        cout<<"Push:Stack Index "<<index<<"--"<<i<<endl;
+    }
+    for (int i = 0; i < 15; i++)
+    {
+        int v;
+        int b = stack_top(v);
+        stack_pop();
+
+        if(b) {
+            cout<<"Stack Index "<<index<<"--"<<v<<endl;
+        }
+        else {
+            cout<<"No more elements in the stack!"<<endl;
+        }
+    }
+    
+    deinit_stacks();
+    return 0;
 
 }
