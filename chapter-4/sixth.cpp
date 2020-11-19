dd873b11432e99331f8c6303f372d56e962e7716
mridul-netizen <mridulsetia18@gmail.com>
2020-11-19 22:03:58 +0530

    updated

--- chapter-4/sixth.cpp
+++
@@ -1,8 +0,0 @@
-//Write a algorithm to find the "next" node(i.e. in-order successor) of a given node in a binary search tree.
-//You may assume that each node has a link to its parent
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-
-}
